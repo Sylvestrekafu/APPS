2d5b4ef5e6d908a97836d765dd6646a253c89238
SYLVESTRE APETCHO <apetchokafu@gmail.com>
2023-07-14 22:03:27 +0200

    add streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Définition des options de délimiteurs
+delimiters = {'Virgule (,)': ',', 'Point-virgule (;)': ';', 'Tabulation (\\t)': '\t', 'Espace ( )': ' '}
 
 # Titre de l'application
 st.title("Mon application complexe")
@@ -32,11 +37,37 @@ st.write("Texte en majuscules :", processed_text)
 # Section 3 de l'application
 st.header("Section 3")
 st.subheader("Téléchargement de fichiers")
-uploaded_file = st.file_uploader("Télécharger un fichier", type=["csv", "txt"])
-if uploaded_file is not None:
-    data = uploaded_file.read()
-    st.write("Contenu du fichier :")
+uploaded_files = st.file_uploader("Télécharger plusieurs fichiers", accept_multiple_files=True, type=["csv"])
+if uploaded_files:
+    delimiter_option = st.selectbox("Choisissez le délimiteur", list(delimiters.keys()))
+    dataframes = [pd.read_csv(file, delimiter=delimiters[delimiter_option]) for file in uploaded_files]
+    data = pd.concat(dataframes)
     st.write(data)
+    if st.button("Tracer le graphique"):
+        # Choix des colonnes pour les axes x et y
+        x_axis = st.selectbox("Choisissez la colonne pour l'axe x", data.columns)
+        y_axis = st.selectbox("Choisissez la colonne pour l'axe y", data.columns)
+
+        # Choix du type de graphique
+        plot_type = st.selectbox("Choisissez le type de graphique",
+                                 ("Scatter Plot", "Line Plot", "Bar Plot", "Histogram", "Area Plot", "Pie Chart"))
+
+        # Choix de la couleur
+        color = st.color_picker("Choisissez une couleur")
+
+        plt.figure(figsize=(12, 6))
+
+        if plot_type == "Scatter Plot":
+            plt.scatter(data[x_axis], data[y_axis], color=color)
+        elif plot_type == "Line Plot":
+            plt.plot(data[x_axis], data[y_axis], color=color)
+        elif plot_type == "Bar Plot":
+            plt.bar(data[x_axis], data[y_axis], color=color)
+
+        plt.xlabel(x_axis)
+        plt.ylabel(y_axis)
+        plt.title(plot_type)
+        st.pyplot()
 
 # Section 4 de l'application
 st.header("Section 4")
@@ -47,4 +78,4 @@ if video_url:
 
 # Footer
 st.markdown("---")
-st.write("Merci d'utiliser notre application  !")
+st.write("Merci d'utiliser notre application !")
